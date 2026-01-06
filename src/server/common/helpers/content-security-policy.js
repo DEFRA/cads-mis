ff774e614628201fca623d46c2906cbcfee6a768
Gary Woodfine <annex-storage-slip@duck.com>
2026-01-06 09:49:47 +0000

    Update Content Security Policy to replace `workerSrc` UUID with `blob:` directive

--- src/server/common/helpers/content-security-policy.js
+++ src/server/common/helpers/content-security-policy.js
@@ -47,7 +47,7 @@ const contentSecurityPolicy = {
     frameAncestors: ['none'],
     formAction: ['self'],
     manifestSrc: ['self'],
-    workerSrc: ['self', '6f4b3df2-fcb7-47e0-9b66-d2f1227db31b'],
+    workerSrc: ['self', 'blob:'],
     generateNonces: false
   }
 }
