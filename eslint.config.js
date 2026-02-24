023dda4355a2612a6f349ee963d6548d4767ee69
David Baillie <david.baillie@esynergy.co.uk>
2026-02-24 15:01:36 +0000

    Merge pull request #6 from DEFRA/develop

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,81 @@
 import neostandard from 'neostandard'
+import vitest from '@vitest/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
+import packageJson from 'eslint-plugin-package-json'
+import globals from 'globals'
 
-export default neostandard({
-  env: ['node', 'vitest'],
-  ignores: [...neostandard.resolveIgnoresFromGitignore()],
-  noJsx: true,
-  noStyle: true
-})
+const customIgnores = [
+  '.server',
+  '.public',
+  'src/__fixtures__',
+  'coverage',
+  '.husky',
+  '.github',
+  'node_modules',
+  '.sonarlint',
+  'raw-assets',
+  '.prettierrc.js',
+  '.vite/setup-files.js',
+  '*.config.*',
+  'vite.config.*',
+  'vitest.config.*'
+]
+
+export default [
+  ...neostandard({
+    env: ['node', 'vitest'],
+    ignores: [...neostandard.resolveIgnoresFromGitignore(), ...customIgnores],
+    noJsx: true,
+    noStyle: true
+  }),
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'module' }
+  },
+  {
+    files: ['**/*.cjs'],
+    languageOptions: { sourceType: 'commonjs' }
+  },
+  {
+    files: ['src/client/**/*.js'],
+    languageOptions: {
+      globals: { ...globals.browser }
+    }
+  },
+  {
+    files: [
+      '.vite/**/*.js',
+      '**/*.test.{js,cjs}',
+      '**/__mocks__/**',
+      '**/__fixtures__/**',
+      'vitest.config.js',
+      'test-helpers/**'
+    ],
+    plugins: { vitest },
+    languageOptions: {
+      globals: { ...vitest.environments.env.globals }
+    },
+    rules: {
+      ...vitest.configs.recommended.rules
+    }
+  },
+  {
+    ...packageJson.configs.recommended,
+    rules: {
+      ...packageJson.configs.recommended.rules,
+      'package-json/restrict-dependency-ranges': [
+        'error',
+        {
+          forDependencyTypes: ['dependencies', 'devDependencies'],
+          rangeType: 'pin'
+        }
+      ]
+    }
+  },
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }]
+    }
+  }
+]
