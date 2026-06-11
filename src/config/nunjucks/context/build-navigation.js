fe0026b54392fd62c6ddb3b52ffe13f559e40538
andy-defra <andy.rose@esynergy.co.uk>
2026-06-11 11:57:39 +0100

    Amendments to home page navigation based on auth

--- src/config/nunjucks/context/build-navigation.js
+++ src/config/nunjucks/context/build-navigation.js
@@ -1,33 +1,25 @@
 export function buildNavigation(request) {
   const isAuthenticated = Boolean(request?.auth?.credentials)
 
-  const items = [
+  if (!isAuthenticated) {
+    return []
+  }
+
+  return [
     {
       text: 'Home',
       href: '/',
       current: request?.path === '/'
-    }
-  ]
-
-  if (isAuthenticated) {
-    items.push({
+    },
+    {
       text: 'Dashboard',
       href: '/dashboard',
       current: request?.path === '/dashboard'
-    })
-
-    items.push({
+    },
+    {
       text: 'Sign out',
       href: '/logout',
       current: false
-    })
-  } else {
-    items.push({
-      text: 'Sign in',
-      href: '/login',
-      current: false
-    })
-  }
-
-  return items
+    }
+  ]
 }
--- src/config/nunjucks/context/build-navigation.test.js
+++ src/config/nunjucks/context/build-navigation.test.js
@@ -10,35 +10,13 @@ describe('#buildNavigation', () => {
       buildNavigation(
         mockRequest({ path: '/non-existent-path', auth: { credentials: null } })
       )
-    ).toEqual([
-      {
-        current: false,
-        text: 'Home',
-        href: '/'
-      },
-      {
-        current: false,
-        text: 'Sign in',
-        href: '/login'
-      }
-    ])
+    ).toEqual([])
   })
 
   test('Should provide expected highlighted navigation details', () => {
     expect(
       buildNavigation(mockRequest({ path: '/', auth: { credentials: null } }))
-    ).toEqual([
-      {
-        current: true,
-        text: 'Home',
-        href: '/'
-      },
-      {
-        current: false,
-        text: 'Sign in',
-        href: '/login'
-      }
-    ])
+    ).toEqual([])
   })
 
   test('Should show protected routes when authenticated', () => {
